0114277358fa1f71a3eff9a9a2438297b55f4d28
henry-dura <henryikemefuna@gmail.com>
2022-12-22 10:25:19 +0100

    Add project card objects

--- index.js
+++ index.js
@@ -14,3 +14,30 @@ cancel.addEventListener('click', () => {
 panel.addEventListener('click', () => {
   menu.classList.remove('absolute');
 });
+
+const array = [
+  {
+      mobileTopic: "tonic",
+      desktopTopic: "tonic",
+      mobileImage: "images/Snapshoot Portfolio.png",
+      desktopImage: "images/desktop-port/new.png",
+  },
+  {
+      mobileTopic: "multi-post stories",
+      desktopTopic: "multi-post stories",
+      mobileImage: "images/Snapshoot Portfolio (1).png",
+      desktopImage: "images/desktop-port/new.png",
+  },
+  {
+      mobileTopic: "tonic",
+      desktopTopic: "facebook 360",
+      mobileImage: "images/Snapshoot Portfolio (2).png",
+      desktopImage: "images/desktop-port/new.png",
+  },
+  {
+      mobileTopic:  "multi-post stories",
+      desktopTopic: "uber navigation",
+      mobileImage: "images/Snapshoot Portfolio (3).png",
+      desktopImage: "images/desktop-port/new.png",
+  }
+]
