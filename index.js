f7c1a8f8a11bf0acdbba76180deba59397d26894
tobby8629 <sarhmullah15@gmail.com>
2022-12-22 01:31:43 -0800

    create the card template

--- index.js
+++ index.js
@@ -41,3 +41,80 @@ const array = [
       desktopImage: "images/desktop-port/new.png",
   }
 ]
+
+const extra = document.querySelector("#extra")
+const project = document.querySelectorAll('#expand');
+
+
+function display (main,i) {
+  let play =  
+` <div class="content">
+      <div class="top">
+        <h2 class="mobile">${main[i].mobileTopic}</h2>
+        <h2 class="desktop">${main[i].desktopTopic}</h2>
+        <ul>
+          <li class="dark desktop">Uber</li>
+          <li class="dark mobile">canopy</li>
+          <li class="desktop">full Stack Dev</li>
+          <li class="mobile">Back End Dev</li>
+          <li class="desktop">2018</li>
+          <li class="mobile">2015</li>
+        </ul>
+      </div>
+      <div class="image">
+        <img src="${main[i].mobileImage}" alt="click to see my Tonic Project" class="mobile"/>
+        <img src="${main[i].desktopImage}" alt="click to see my Tonic Project" class="desktop"/>
+      </div>
+      <div class="bottom">
+        <div class="words">
+          <p class="desktop">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type 
+            and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it 1960s with the relea
+          </p>
+
+          <p class="mobile">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+              when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+             It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent
+          </p>
+        </div>
+         
+        <div class="other">
+          <ul class="stack">
+            <li>html</li>
+            <li>javascript</li>
+            <li class="desktop">ruby </li>
+            <li>css</li>
+            <li class="desktop">github</li>
+            <li class="desktop">bootstrap</li>
+          </ul>
+  
+          <div class="button">
+            <button class="btn">
+              see live
+              <img src="images/expand/seelive.png" alt="visit the website"/>
+            </button>
+            <button class="btn">
+              see source
+              <img src="images/expand/seesource.png" alt=" see the sourcecode"/>
+            </button>
+          </div>
+        </div>
+        
+        
+
+        <img src="images/expand/cancel.png" alt="To close panel" id="cancel-port"/>
+        
+      </div>`
+ 
+  const lay = document.createElement('div');
+  lay.classList.add('popup');
+  lay.innerHTML = play;
+  extra.appendChild(lay);
+}
